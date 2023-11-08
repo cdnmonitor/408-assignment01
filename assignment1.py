6cbb48cd72e5d23585cb05e01bf97aa5cc77c8f8
michael bishai <monitorthat@gmail.com>
2023-11-07 19:26:36 -0800

    assignment1

--- assignment1.py
+++ assignment1.py
@@ -2,7 +2,8 @@ import sqlite3
 import csv
 import os
 
-DATABASE_NAME = 'StudentDB.sqlite'
+DATABASE_NAME = "StudentDB.sqlite"
+
 
 # create connection
 def create_connection(db_file):
@@ -18,7 +19,8 @@ def create_connection(db_file):
 def create_table(conn):
     try:
         cur = conn.cursor()
-        cur.execute('''
+        cur.execute(
+            """
         CREATE TABLE IF NOT EXISTS Students (
             StudentId INTEGER PRIMARY KEY,
             FirstName TEXT,
@@ -33,13 +35,28 @@ def create_table(conn):
             MobilePhoneNumber TEXT,
             isDeleted INTEGER
         )
-        ''')
+        """
+        )
         conn.commit()
     except sqlite3.Error as e:
         print(e)
 
+
+def clean_phone_number(phone_number):
+    # Remove any character that is not a digit or x for extension
+    cleaned_number = "".join(
+        filter(lambda x: x.isdigit() or x.lower() == "x", phone_number)
+    )
+
+    # Remove leading 1 if the length is greater than 10 after cleaning
+    if len(cleaned_number) > 10 and cleaned_number.startswith("1"):
+        cleaned_number = cleaned_number[1:]
+
+    return cleaned_number
+
+
 def import_from_students_csv(conn):
-    csv_file_name = 'students.csv'
+    csv_file_name = "students.csv"
     if os.path.isfile(csv_file_name):
         try:
             import_students_from_csv(csv_file_name, conn)
@@ -48,32 +65,40 @@ def import_from_students_csv(conn):
     else:
         print("The 'students.csv' file does not exist in the current directory.")
 
-def import_students_from_csv(csv_file_path, conn):
 
+def import_students_from_csv(csv_file_path, conn):
     try:
-        with open(csv_file_path, 'r') as csvfile:
+        with open(csv_file_path, "r") as csvfile:
             csv_reader = csv.DictReader(csvfile)
-            students_to_insert = [
-                (
-                    row['FirstName'],
-                    row['LastName'],
-                    row['Address'],
-                    row['City'],
-                    row['State'],
-                    row['ZipCode'],
-                    row['MobilePhoneNumber'],
-                    row['Major'],
-                    float(row['GPA']),
-                    0  # Assuming isDeleted is 0 for new entries
+            students_to_insert = []
+            for row in csv_reader:
+                # clean the phone number before inserting
+                cleaned_phone_number = clean_phone_number(row["MobilePhoneNumber"])
+
+                students_to_insert.append(
+                    (
+                        row["FirstName"],
+                        row["LastName"],
+                        row["Address"],
+                        row["City"],
+                        row["State"],
+                        row["ZipCode"],
+                        cleaned_phone_number,  # cleaned and fixed phone number
+                        row["Major"],
+                        float(row["GPA"]),
+                        0,  # isDeleted entry
+                    )
                 )
-                for row in csv_reader
-            ]
+
             cur = conn.cursor()
-            cur.executemany('''
+            cur.executemany(
+                """
                 INSERT INTO Students 
                 (FirstName, LastName, Address, City, State, ZipCode, MobilePhoneNumber, Major, GPA, isDeleted) 
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-            ''', students_to_insert)
+            """,
+                students_to_insert,
+            )
             conn.commit()
             print(f"Inserted {cur.rowcount} rows into Students table")
     except sqlite3.Error as e:
@@ -86,7 +111,7 @@ def import_students_from_csv(csv_file_path, conn):
 
 # Function to display all students
 def display_all_students(conn):
-    """ Display all students and their attributes """
+    """Display all students and their attributes"""
     cur = conn.cursor()
     query = "SELECT * FROM Students WHERE isDeleted=0"
     cur.execute(query)
@@ -96,77 +121,250 @@ def display_all_students(conn):
         print(row)
 
 
-# Function to add a new student
-def add_new_student(conn):
+# Helper function to get validated input
+def get_validated_input(prompt_message, error_message, validation_function):
+    while True:
+        user_input = input(prompt_message).strip()
+        if validation_function(user_input):
+            return user_input
+        else:
+            print(error_message)
+
+
+def is_valid_gpa(gpa):
     try:
-        first_name = input("Enter first name: ")
-        last_name = input("Enter last name: ")
-        major = input("Enter major: ")
-        faculty_advisor = input("Enter faculty advisor: ")
-        address = input("Enter address: ")
-        city = input("Enter city: ")
-        state = input("Enter state: ")
-        zip_code = input("Enter zip code: ")
-        mobile_phone_number = input("Enter mobile phone number: ")
+        value = float(gpa)
+        return 0.0 <= value <= 4.0
+    except ValueError:
+        return False
 
-        while True:  # wait for valid input
-            gpa_input = input("Enter GPA: ")
-            try:
-                gpa = float(gpa_input)  # Attempt to convert input to a float
-                if gpa < 0.0 or gpa > 4.0:  #real gpas only
-                    print("GPA must be between 0.0 and 4.0")
-                else:
-                    break  # gpa is valid
-            except ValueError:
-                print("Invalid input for GPA. Please enter a numeric value.")  # retry
 
-        try:
-            cur = conn.cursor()
-            cur.execute('''
-                INSERT INTO Students 
-                (FirstName, LastName, GPA, Major, FacultyAdvisor, Address, City, State, ZipCode, MobilePhoneNumber, isDeleted) 
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-            ''', (
-            first_name, last_name, gpa, major, faculty_advisor, address, city, state, zip_code, mobile_phone_number, 0))
-            conn.commit()
-            print("New student added successfully.")
-        except sqlite3.Error as e:
-            print(f"An error occurred: {e}")
+def is_valid_address(address):
+    return len(address) <= 100
+
+
+def is_valid_zip_code(zip_code):
+    return zip_code.isdigit() and len(zip_code) == 5
+
+
+def is_valid_phone_number(phone):
+    return phone.isdigit() and len(phone) == 10
+
+
+def is_valid_state(state):
+    valid_states = [
+        "AL",
+        "AK",
+        "AZ",
+        "AR",
+        "CA",
+        "CO",
+        "CT",
+        "DE",
+        "FL",
+        "GA",
+        "HI",
+        "ID",
+        "IL",
+        "IN",
+        "IA",
+        "KS",
+        "KY",
+        "LA",
+        "ME",
+        "MD",
+        "MA",
+        "MI",
+        "MN",
+        "MS",
+        "MO",
+        "MT",
+        "NE",
+        "NV",
+        "NH",
+        "NJ",
+        "NM",
+        "NY",
+        "NC",
+        "ND",
+        "OH",
+        "OK",
+        "OR",
+        "PA",
+        "RI",
+        "SC",
+        "SD",
+        "TN",
+        "TX",
+        "UT",
+        "VT",
+        "VA",
+        "WA",
+        "WV",
+        "WI",
+        "WY",
+    ]
+    return state.upper() in valid_states
+
+
+# Validation function for names allowing space
+def is_valid_name(name):
+    return all(x.isalpha() or x.isspace() for x in name) and len(name) <= 50
+
+
+# Validation function for majors allowing letters, numbers, spaces, and some special characters
+def is_valid_major(major):
+    return (
+        all(x.isalnum() or x.isspace() or x in "-&" for x in major) and len(major) <= 50
+    )
+
+
+# Function to add a new student with input validation and retry, not allowing blank entries
+def add_new_student(conn):
+    first_name = get_validated_input(
+        "Enter first name: ",
+        "Invalid first name. Must be alphabetic, non-empty, and less than 50 characters.",
+        # if input is not an empty string and valid name function
+        lambda input: input and is_valid_name(input),
+    )
+    last_name = get_validated_input(
+        "Enter last name: ",
+        "Invalid last name. Must be alphabetic, non-empty, and less than 50 characters.",
+        lambda input: input and is_valid_name(input),
+    )
+    gpa = get_validated_input(
+        "Enter GPA: ",
+        "Invalid GPA. Must be a non-empty numeric value between 0.0 and 4.0.",
+        lambda input: input and is_valid_gpa(input),
+    )
+    major = get_validated_input(
+        "Enter major: ",
+        "Invalid major. Must have char, letters, numbers, spaces, hyphens, and ampersands.",
+        lambda input: input and is_valid_major(input),
+    )
+    address = get_validated_input(
+        "Enter address: ",
+        "Invalid address. Must be non-empty and less than 100 characters.",
+        lambda input: input and is_valid_address(input),
+    )
+    city = get_validated_input(
+        "Enter city: ",
+        "Invalid city. Must be alphabetic, non-empty, and less than 50 characters.",
+        lambda input: input and is_valid_name(input),
+    )
+    state = get_validated_input(
+        "Enter state abbreviation (e.g., CA for California): ",
+        "Invalid state abbreviation. Must be non-empty and a two-letter code such as 'CA'.",
+        lambda input: input and is_valid_state(input),
+    )
+    zip_code = get_validated_input(
+        "Enter zip code: ",
+        "Invalid zip code. Must be non-empty and 5 digits.",
+        lambda input: input and is_valid_zip_code(input),
+    )
+    mobile_phone_number = get_validated_input(
+        "Enter mobile phone number: ",
+        "Invalid phone number. Must be non-empty and 10 digits without any spaces or symbols.",
+        lambda input: input and is_valid_phone_number(input),
+    )
+
+    # Convert GPA to float after validation
+    gpa = float(gpa)
+
+    try:
+        cur = conn.cursor()
+        cur.execute(
+            """
+            INSERT INTO Students 
+            (FirstName, LastName, GPA, Major, Address, City, State, ZipCode, MobilePhoneNumber, isDeleted) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """,
+            (
+                first_name,
+                last_name,
+                gpa,
+                major,
+                address,
+                city,
+                state,
+                zip_code,
+                mobile_phone_number,
+                0,
+            ),
+        )
+        conn.commit()
+        print("New student added successfully.")
+    except sqlite3.Error as e:
+        print(f"Database error: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
 
 
 # Function to update a student
 def update_student(conn):
-    # update student details
-    student_id = input("Enter student ID to update: ")
-    new_major = input("Enter new major (leave blank to not update): ")
-    new_advisor = input("Enter new advisor (leave blank to not update): ")
-    new_mobile_phone_number = input("Enter new mobile phone number (leave blank to not update): ")
+    student_id = input("Enter student ID to update: ").strip()
+    if not student_id.isdigit():
+        print("Invalid student ID. Please enter a numeric value.")
+        return
 
     cur = conn.cursor()
-    update_data = []
-    query = "UPDATE Students SET "
-    if new_major:
-        query += "Major=?, "
-        update_data.append(new_major)
-    if new_advisor:
-        query += "FacultyAdvisor=?, "
-        update_data.append(new_advisor)
-    if new_mobile_phone_number:
-        query += "MobilePhoneNumber=?, "
-        update_data.append(new_mobile_phone_number)
 
-    # Remove extra comma and space
-    query = query.rstrip(', ')
-    query += " WHERE StudentId=? AND isDeleted=0"
-    update_data.append(student_id)
-
-    cur.execute(query, update_data)
-    conn.commit()
-
-    if cur.rowcount > 0:
-        print("Student updated successfully.")
-    else:
+    # Check if student exists and is not deleted
+    cur.execute("SELECT * FROM Students WHERE StudentId = ? AND isDeleted = 0", (student_id,))
+    student = cur.fetchone()
+    if not student:
         print("No student found with the given ID, or the student is deleted.")
+        return
+
+    print("Select an attribute to update:")
+    attributes = {
+        "1": "FirstName",
+        "2": "LastName",
+        "3": "GPA",
+        "4": "Major",
+        "5": "FacultyAdvisor",
+        "6": "Address",
+        "7": "City",
+        "8": "State",
+        "9": "ZipCode",
+        "10": "MobilePhoneNumber",
+    }
+    for key, value in attributes.items():
+        print(f"{key}. {value}")
+    choice = input("Enter the number of the attribute: ")
+
+    attribute = attributes.get(choice)
+    if not attribute:
+        print("Invalid choice.")
+        return
+
+    # Get the new value for the chosen attribute
+    new_value = input(f"Enter the new value for {attribute}: ")
+
+    # If the attribute is GPA, validate it's a float
+    if attribute == "GPA" and not is_valid_gpa(new_value):
+        print("Invalid GPA. Must be a numeric value between 0.0 and 4.0.")
+        return
+
+    # If the attribute is MobilePhoneNumber, validate it's a correct phone number
+    if attribute == "MobilePhoneNumber" and not is_valid_phone_number(new_value):
+        print("Invalid phone number. Must be 10 digits without any spaces or symbols.")
+        return
+
+    # Update the database
+    update_query = f"UPDATE Students SET {attribute} = ? WHERE StudentId = ? AND isDeleted = 0"
+
+    try:
+        cur.execute(update_query, (new_value, student_id))
+        conn.commit()
+        if cur.rowcount > 0:
+            print(f"Student {attribute} updated successfully to {new_value}.")
+        else:
+            print("No updates were made. Please check the student ID and try again.")
+    except sqlite3.Error as e:
+        print(f"Database error: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
 
 
 # delete student by StudentId function
@@ -183,21 +381,56 @@ def delete_student(conn):
         print("No student found with the given ID.")
 
 
-# display students function
 def search_students(conn):
-    # Search students by major, GPA, city, state, and advisor
-    search_query = input("Enter search query (Major, GPA, City, State, Advisor): ")
+    # choose an attribute to search by
+    print("Select an attribute to search by:")
+    print("1. First Name")
+    print("2. Last Name")
+    print("3. Major")
+    print("4. City")
+    print("5. State")
+    print("6. GPA")
+    print("7. Faculty Advisor")
+    print("8. Mobile Phone Number")
+    choice = input("Enter the number of the attribute: ")
+
+    search_attributes = {
+        "1": ("FirstName", is_valid_name),
+        "2": ("LastName", is_valid_name),
+        "3": ("Major", is_valid_major),
+        "4": ("City", is_valid_name),
+        "5": ("State", is_valid_state),
+        "6": ("GPA", is_valid_gpa),
+        "7": ("FacultyAdvisor", is_valid_name),
+        "8": ("MobilePhoneNumber", is_valid_phone_number),
+    }
+
+    if choice not in search_attributes:
+        print("Invalid choice. Please enter a number between 1 and 8.")
+        return
+
+    attribute, validation_function = search_attributes[choice]
+    search_query = get_validated_input(
+        f"Enter search query for {attribute}: ",
+        f"Invalid input for {attribute}.",
+        lambda input: input and validation_function(input)
+    )
+
     cur = conn.cursor()
-    cur.execute('''
-        SELECT * FROM Students 
-        WHERE (Major LIKE ? OR GPA LIKE ? OR City LIKE ? OR State LIKE ? OR FacultyAdvisor LIKE ?) 
-        AND isDeleted=0
-    ''', ('%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%',
-          '%' + search_query + '%'))
+
+    if attribute == "GPA":
+        # exact match instead of a LIKE query
+        cur.execute(f"SELECT * FROM Students WHERE {attribute} = ? AND isDeleted = 0", (search_query,))
+    else:
+        # LIKE for a simple pattern match
+        cur.execute(f"SELECT * FROM Students WHERE {attribute} LIKE ? AND isDeleted = 0", (f"%{search_query}%",))
 
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    if rows:
+        for row in rows:
+            print(row)
+    else:
+        print(f"No results found for {search_query} in {attribute}.")
 
 
 def main():
@@ -231,16 +464,16 @@ def main():
             elif choice == "6":
                 import_from_students_csv(conn)
             elif choice == "7":
-                print("Exiting the application.")
+                print("Exiting.")
                 break
             else:
                 print("Invalid choice. Please enter a number between 1 and 7.")
     else:
         print("Error! Cannot create the database connection.")
 
-    if conn:  # Check if connection was successfully created
+    if conn:  # close in case
         conn.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- readme.md
+++ readme.md
@@ -6,12 +6,21 @@ Michael Bishai
 CPSC 408 01
 ### Notes:
 * Everything is written in functions for sake of organization and for a clean main function.
-
+  * this is a later edit, when I finished adding search by phone I realized how much more I had to worry about.
+  * I added more formatting for phone number after that fact, but for the most part this is function focused code.
 * Error handling sqlite3 connections:
   * https://stackoverflow.com/questions/25371636/how-to-get-sqlite-result-error-codes-in-python
 * Reading a CSV file using the DictReader class
     * https://www.pythontutorial.net/python-basics/python-read-csv-file/
 * Handling error conditions
   * https://docs.python.org/3/library/exceptions.html
+  * https://www.educba.com/python-validation/
 * Student Menu
   * https://www.geeksforgeeks.org/python-program-to-sort-and-find-the-data-in-the-student-records/
+* Placeholders
+  * https://docs.python.org/3/library/sqlite3.html#:~:text=placeholders%20are%20used%20to%20bind,SQL%20queries%20for%20more%20details).
+
+Some of the challenges were:
+* How to format mid csv import
+* how to format, use lambda filtering along with function based filtering
+* How to build menu options and how to make it easier to update entries.
